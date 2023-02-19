ec418e1efcf85e745bc02c5532c599f480740371
Joseph <benabdeljelil.youssef@gmail.com>
2023-02-19 15:27:52 +0100

    init

---
+++ app/src/main/java/com/example/mynoteapp/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mynoteapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mynoteapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
